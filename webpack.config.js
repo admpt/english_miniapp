c30f966bbcc2dc46ce171e9c67f56c025c19fdbe
Andrey Dmitrievich <developerpro0py@gmail.com>
2024-11-10 15:11:25 +0500

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');  // Плагин для генерации HTML
 
 module.exports = {
   entry: './docs/index.js',  // Путь к вашему исходному файлу в папке docs
   output: {
     filename: 'main.js',  // Название выходного файла
-    path: path.resolve(__dirname, 'docs'),  // Папка для выходных файлов
+    path: path.resolve(__dirname, 'docs'),  // Папка для выходных файлов (docs)
   },
   module: {
     rules: [
@@ -14,7 +15,7 @@ module.exports = {
         use: 'babel-loader',  // Используем Babel для транспиляции
       },
       {
-        test: /\.css$/,  // Стили
+        test: /\.css$/,  // Преобразуем CSS файлы
         use: ['style-loader', 'css-loader'],
       },
     ],
@@ -23,8 +24,14 @@ module.exports = {
     extensions: ['.js', '.jsx'],  // Разрешаем .js и .jsx файлы
   },
   devServer: {
-    static: path.resolve(__dirname, 'docs'),  // Статика из папки docs
+    static: path.resolve(__dirname, 'docs'),  // Папка с результатами сборки (docs)
     port: 8000,  // Порт для сервера
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './docs/index.html',  // Путь к вашему шаблону HTML в папке docs
+      filename: 'index.html',  // Генерируем файл index.html в выходной папке (docs)
+    }),
+  ],
   mode: 'development',  // Режим разработки
 };
